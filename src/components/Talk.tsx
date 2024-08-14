859ade4d34556a3354f792d6fc43aae20af19f7d
J-dev740 <tcr19ec029@gectcr.ac.in>
2024-08-14 07:55:38 +0530

    bug fix

--- src/components/Talk.tsx
+++ src/components/Talk.tsx
@@ -27,7 +27,7 @@ export const Talk = () => {
   return (
     <Section title={"Let's Talk"}>
         {/* contact form  */}
-         <motion.form 
+         <form 
          ref={ref}
          onSubmit={handleSubmit} className='flex flex-col gap-y-3 w-full min-w-[200px] max-w-[800px] md:text-[22px] text-[15px] font-gae placeholder-[#8C8FA6] text-black font-medium'>
                 <p className='flex  flex-wrap w-fit self-center justify-self-center hover:underline  transition-all duration-200 font-gae text-[38px] text-[#474747] max-[810px]:text-[22px]  font-extralight underline-offset-2'>jithu4cfs@gmail.com</p>
@@ -38,26 +38,34 @@ export const Talk = () => {
                       </div>
             <div className='flex w-full max-[810px]:flex-col items-center justify-center gap-4  mt-[80px] '>
                 <motion.input 
-                 initial={{x:-1000}}
-                 animate={isInView?{x:0}:{x:-1000}}
-                 transition={{duration:0.5,delay:1}}
+                //  initial={{x:-1000}}
+                //  animate={isInView?{x:0}:{x:-1000}}
+                //  transition={{duration:0.5,delay:0.5}}
+                //  viewport={{once:false,amount:0.3}}
+                initial={{opacity:0}}
+                animate={isInView?{opacity:1}:{opacity:0}}
+                transition={{duration:2,delay:0.5}}
                 required={true} name='user_name' type='text' placeholder='Your Name' className='border-[2px] h-[45px] w-1/2 pl-2 border-[#474747] rounded-[15px]  '/>
                 <motion.input
-                initial={{x:1000}}
-                animate={isInView?{x:0}:{x:1000}}
-                transition={{duration:0.5,delay:0.5}}
+                // initial={{x:1000}}
+                // animate={isInView?{x:0}:{x:1000}}
+                // transition={{duration:0.5,delay:0.5}}
+                // viewport={{once:false,amount:0.3}}
+                initial={{opacity:0}}
+                animate={isInView?{opacity:1}:{opacity:0}}
+                transition={{duration:2,delay:0.5}}
                  required={true} name='user_email' type='email' placeholder='Email Address' className='border-[2px] pl-2  w-1/2 h-[45px] border-[#474747] rounded-[15px] '/>
             </div>
-            <motion.textarea 
-            initial={{y:1000}}
-            animate={isInView?{y:0}:{y:1000}}
-            transition={{duration:0.5,delay:1}}
+            <textarea 
+            // initial={{y:1000}}
+            // animate={isInView?{y:0}:{y:1000}}
+            // transition={{duration:0.5,delay:1}}
             name='message' className='flex min-[810px]:min-h-[100px] pl-3 w-full rounded-[20px] border-[#474747] border-[2px]'/>
             <div className='flex bg-black rounded-[20px] '>
             <button type='submit'  className='flex w-full max-w-[800px] hover:-translate-x-1 hover:-translate-y-1 transition-transform duration-300 rounded-[20px] h-[40px] text-white font-gae bg-[#474747] text-center items-center justify-center'>Send</button>
             </div>
 
-         </motion.form>
+         </form>
     </Section>
   )
 }
